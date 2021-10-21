ad502269ddd2c985bfd13c04f68519b5f143ee9f
Per Jensen <per2jensen@gmail.com>
2021-10-21 09:29:25 +0200

    Dockerfile path fix

--- test/build-run.sh
+++ test/build-run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker build --tag pj/fondskode -f ../test/Dockerfile
+docker build --tag pj/fondskode -f ../src/Dockerfile
 docker run --net hassio --rm  --name fondskode-getter -it pj/fondskode:latest
