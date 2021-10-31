e6b74e5d674ffc681745c7a49f4ac6431a3e6d52
Per Jensen <per2jensen@gmail.com>
2021-10-31 08:07:52 +0100

    fix systemd service

---
+++ src/usr/local/bin/fondskode.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+docker run \
+  -e FONDSKODE='952737' \
+  -e TOTALKREDIT_URL='https://netbank.totalkredit.dk/netbank/showStockExchangeInternal.do' \
+  -e INFLUX_HOST='<host for influxdb>' \
+  -e INFLUX_USER='<Influxdb user>' \
+  -e INFLUX_PASS='<Influxdb user password>' \
+  -e INFLUX_DB='<influxdb DB>' \
+  --net <navn på docker netværk, hvis influxdb kører i en container> \
+  --rm  \
+  -it \
+  per2jensen/fondskode:latest
+
